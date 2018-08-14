2596bc2de3bb824139d6ad92dfd3cdcd6d5bc39f
rajakokilarajasekar <40596633+rajakokilarajasekar@users.noreply.github.com>
2018-08-14 10:35:34 +0000

    Create min.c

---
+++ min.c
@@ -0,0 +1,21 @@
+Min
+#include<stdio.h>
+int main()
+{
+int a[10],i,j,temp,n;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+temp=a[0];
+for(i=0;i<n;i++)
+{
+if(temp>a[i])
+{
+temp=a[i];
+}
+}
+printf("%d",temp);
+return 0;
+}
